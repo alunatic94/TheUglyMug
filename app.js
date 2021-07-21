645b53aca70495ce26fd72d9e594f9e9819559c8
alunatic94 <Alberto.luna.642@my.csun.edu>
2021-07-20 17:10:45 -0700

    added carousel slider

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Testing");
+const carouselImages=document.querySelector('.carousel__images');
+const carouselButtons=document.querySelectorAll('.carousel_button');
+const numberOfImages=document.querySelectorAll('.carousel__images img').length;
+let imageIndex=1;
+let translateX=0;
+
+carouselButtons.forEach(button =>{
+    button.addEventListener('click',event=>{
+        if(event.target.id==='previous'){
+            if(imageIndex!==1){
+                imageIndex--;
+                translateX+=300;
+            }
+        }
+        else{
+            if(imageIndex!==numberOfImages-3){
+                imageIndex++;
+                translateX-=300;
+            }
+        }
+        carouselImages.style.transform=`translate(${translateX}px)`;
+        console.log(imageIndex);
+    });
+});
